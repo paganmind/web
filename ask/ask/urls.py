98914bc2b2c7c2a67df0ca170b97ca7862bbe458
PaganMind <gleb.bezruchkin@gmail.com>
2018-10-15 12:25:37 +0000

    routing && gunicorn

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from qa.views import test
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    url(r'^$', test),
+    url(r'^login/$', test),
+    url(r'^signup/$', test),
+    url(r'^question/(?P<question_id>[0-9]+)/$', test),
+    url(r'^ask/$', test),
+    url(r'^popular/$', test),
+    url(r'^new/$', test)
 ]
--- gunicorn.sh
+++ gunicorn.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cd ~/stepik_web/web && gunicorn -b 0.0.0.0:8080 hello:wsgi_app
-#cd ~/web && gunicorn -b 0.0.0.0:8080 hello:wsgi_app
+#cd ~/stepik_web/web && gunicorn -b 0.0.0.0:8080 hello:wsgi_app
+cd ~/web && gunicorn -b 0.0.0.0:8080 hello:wsgi_app
+cd ~/web && gunicorn -b 0.0.0.0:8000 ./ask/ask/wsgi.py:application
