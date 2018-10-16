fe926c431ad9fe96ee63664f694dc9cb3b1b6f12
PaganMind <gleb.bezruchkin@gmail.com>
2018-10-16 14:03:08 +0000

    migrations2

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':'db_web',
         'USER': 'wepwawet',
-        'PASSWORD': 'password'
+        'PASSWORD': 'password',
 	'OPTIONS': { 
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
 	}
